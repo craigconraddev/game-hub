bffea81a5fd57f5186c60912e4bb0fca0ef8fcfe
Craig Conrad <curion.dev@gmail.com>
2026-04-29 23:32:17 -0400

    added theme and theme button to navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import React, { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "@/components/ui/provider";
 import "./index.css";
 import App from "./App.tsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import { system } from "./theme.ts";
+import { ColorModeProvider } from "./components/ui/color-mode.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <ChakraProvider value={system}>
+      <ColorModeProvider defaultTheme="dark">
+        <App />
+      </ColorModeProvider>
+    </ChakraProvider>
   </StrictMode>,
 );
