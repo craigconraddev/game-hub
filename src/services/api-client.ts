407576c94faafb6dee83c1b23375ac82ac1e6167
Craig Conrad <curion.dev@gmail.com>
2026-05-02 17:31:26 -0400

    hide api key from github commits

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
+    baseURL: 'https://rawg.io/api',
     params: {
-        key: ''
+        // Vite looks for variables starting with VITE_
+        key: import.meta.env.VITE_RAWG_KEY 
     }
 })
