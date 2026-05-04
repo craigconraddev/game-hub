5e31ec7a77d99276e467eef510a5e548c406ecf8
Craig Conrad <curion.dev@gmail.com>
2026-05-03 23:00:46 -0400

    fixed deployment error

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import type { Game } from "@/hooks/useGames";
-import { Card, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, Heading, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 
 interface Props {
